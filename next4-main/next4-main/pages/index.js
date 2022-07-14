dfda6db89ea2c55326166208c4766fd27a0cc4ab
Marcelo1403 <marcelovictormelonunes@gmail.com>
2022-07-14 10:10:33 -0300

    add next4

---
+++ next4-main/next4-main/pages/index.js
@@ -0,0 +1,17 @@
+// http://localhost:3000
+
+export default function Main() {
+
+    return (
+
+        <div>
+
+            <a href="/movies2"> ACESSAR FILMES </a>
+
+        </div>
+
+    )
+
+}
+
+
